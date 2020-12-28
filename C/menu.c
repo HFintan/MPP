fd7069779a4eb1e77dc4e14332e10b46e7178f24
Fintan Hegarty <fintan.hegarty@gmail.com>
2020-12-28 06:39:56 -0800

    live C works

--- C/menu.c
+++ C/menu.c
@@ -6,9 +6,10 @@
 // Then want an optional argument user file
 // This user file will issue a "demand" to the shop
 
-// Global variable
-float total;
-float budget;
+// Global variables
+float livetotal;
+float livebudget;
+float escape; 
 
 ///////////////////////////////////////////////////
 //// Read in shop file and extract info
@@ -29,6 +30,18 @@ struct Shop {
 	struct ProductStock stock[20];
 	int index;
 };
+
+struct Desires {
+	char* item;
+	int quantity;
+};
+
+struct Customer {
+	double funds;
+	struct Desires desire;
+	char* name;
+};
+
 
 // Read in shop file
 struct Shop createAndStockShop()
@@ -84,21 +97,20 @@ void printShop(struct Shop s)
 	}
 }
 
-//// Come back later and make these more flexible - i.e. not hardcoded. FIXME
-////const char *items[] = {"Apple","Bread","Coke","Dogfood","Ethereum"};
 
 //////////////////////////////////////////////////
+//////////// FROM CSV FILES //////////////////////
+//////////////////////////////////////////////////
+
 // Read in customer file
 
 // Extract info from customer file
 
-// Update shop file
 
 
 //////////////////////////////////////////////////////////////
 ///////////////////////// Live option ////////////////////////
 //////////////////////////////////////////////////////////////
-/// Will run this if, perhaps no optional argument given /////
 
 
 // Find customer name, and, more importantly, budget.
@@ -109,56 +121,65 @@ void meetNgreet()
 	fgets(name,20,stdin);
 	
 	printf("\nPlease disclose your budget, %s",name);
-	scanf("%f",&budget);
+	scanf("%f",&livebudget);
 	
-	if (budget >= 0.55) 
-		{printf("\n%.2f, eh? Please come in!\n",budget);}
+	if (livebudget >= 0.55) 
+		{printf("\n%.2f, eh? Please come in!\n",livebudget);}
 	else
 		{printf("You may have better luck elsewhere.\n");}
 }
 
 
 // Prints menu
-void printMenu()
+//  In hindsight, I should have used the struct for these, but 
+//  they're not going to change. The method for updating the quantity
+//  would carry over anyway.
+void printMenu(struct Shop* shop)
 {
-	printf("These are our wares. What would you like?\n");
-	printf("ITEM                      COST\n");
-	printf("------------------------------\n");
-	printf("Apple                     0.55\n");
-	printf("Bread                     0.80\n");
-	printf("Coke                      1.00\n");
-	printf("Dogfood  		 10.00\n");
-	printf("Ethereum	        100.00\n");
-	printf("------------------------------\n");
+	printf("\n------------------------------------------------\n");
+	printf("ITEM                      COST          IN STOCK\n");
+	printf("--------------------------------------------------\n");
+	printf("Apple                     0.55                %d\n",(*shop).stock[0].quantity);
+	printf("Bread                     0.80                %d\n",(*shop).stock[1].quantity);
+	printf("Coke                      1.00                %d\n",(*shop).stock[2].quantity);
+	printf("Dogfood  		 10.00                %d\n",(*shop).stock[3].quantity);
+	printf("Ethereum	        100.00                %d\n",(*shop).stock[4].quantity);
+	printf("-------------------------------------------------\n");
 }
 
 // Show menu and ask what they want. 
 // Split into separate name and quantity functions
-requestName(struct Shop shop)
+requestName(struct Shop* shop)
 {
 	char menu_option[20]; 
 	int want;
-	printMenu();
-	printf("What would you like?\n");
+	printMenu(shop);
+	printf("What would you like? If you wish to exit, press X.\n");
 	scanf("%s", menu_option);
 	printf("You requested %s\n", menu_option);
-
-	int i = 0;
-	int inLuck = 0;
-	while (i<4) {
-		if (strcmp(shop.stock[i].product.name, menu_option) == 0){
-			want = i;
-			printf("%s? We sell those!\n", shop.stock[i].product.name);
-			inLuck = 1;
-			break;
+	
+	if (strcmp(menu_option, "X")==0){
+		printf("EXIT\n");
+		return 7; 
+	}
+	else{		
+		int i = 0;
+		int inLuck = 0;
+		while (i<5) {
+			if (strcmp((*shop).stock[i].product.name, menu_option) == 0){
+				want = i;
+				printf("%s? We sell those!\n", (*shop).stock[i].product.name);
+				inLuck = 1;
+				break;
+			}
+		i++;
 		}
-	i++;
+		if (inLuck == 0){
+			printf("Sorry, that doesn't appear to be an option. Please check your spelling.\n");
+			return 6; // greater than the number of items in the list
+		}
+		return want;
 	}
-	if (inLuck == 0){
-		printf("Sorry, that doesn't appear to be an option. Please check your spelling.\n");
-		return 6; // greater than the number of items in the list
-	}
-	return i;
 }
 
 // Ask how many they want.
@@ -170,49 +191,53 @@ requestNumber()
 	return howmany;
 }
 
-double makeRequest(double total, struct Shop shop)
+// Try to process request.
+double makeRequest(double livetotal, struct Shop* shop)
 {
 	int requestItem;
 	int requestQuantity;
 	double subtotal;
 	requestItem = requestName(shop);
-	if (requestItem != 6)
+	if (requestItem < 6)
 		{requestQuantity = requestNumber();
-		printf("You want %d %s.\n", requestQuantity, shop.stock[requestItem].product.name);
-		if (requestQuantity > shop.stock[requestItem].quantity)
-			{printf("I'm afraid we only have %d %s. Request denied.\n", shop.stock[requestItem].quantity, shop.stock[requestItem].product.name);
+		printf("You want %d %s.\n", requestQuantity, (*shop).stock[requestItem].product.name);
+		if (requestQuantity > (*shop).stock[requestItem].quantity)
+			{printf("I'm afraid we only have %d %s. Request denied.\n", (*shop).stock[requestItem].quantity, (*shop).stock[requestItem].product.name);
 		}
 		else 
-			{subtotal = requestQuantity * shop.stock[requestItem].product.price; 
+			{subtotal = requestQuantity * (*shop).stock[requestItem].product.price; 
 			float pretotal;
-			pretotal = total + subtotal;
-			if (pretotal < budget)
+			pretotal = livetotal + subtotal;
+			if (pretotal <= livebudget)
 				{printf("That will cost %.2f. Your total is now %.2f\n", subtotal, pretotal);
-				total = pretotal;
-				printf("ARGH %d",shop.stock[requestItem].quantity-requestQuantity);
-				//shop->stock[requestItem].quantity=shop.stock[requestItem].quantity-requestQuantity;
+				livetotal = pretotal;
+				shop->stock[requestItem].quantity=(*shop).stock[requestItem].quantity-requestQuantity;
 			}
 			else
-				{printf("That would cost %.2f. Your total is already %.2f. Your budget is %.2f. Request denied.", subtotal, total, budget);
+				{printf("That would cost %.2f. Your total is %.2f. Your budget is %.2f. Request denied.", subtotal, livetotal, livebudget);
 			}
 		}
-		return total;
+		return livetotal;
+	}
+	else if (requestItem == 7){
+		escape = 1;
+	}
+	else if (requestItem == 6){
+		return livetotal;
 	}
 }
 
+/////////////////////////////////////////////////////////
 
-
-// Calculate and check whether customer can afford; update their budget and total if so.
-//void cashRegister{
-//
-//}
-
-
-void doingItLive(struct Shop shop)
+void doingItLive(struct Shop *shop)
 {
 	meetNgreet();
-	while (total + 0.55 < budget)
-		{total = makeRequest(total,shop);}
+	while (livetotal + 0.55 <= livebudget && escape == 0)
+		{livetotal = makeRequest(livetotal,shop);}
+	float shopCash;
+	printf("Thank you, goodbye!\n");
+	printf("The shop now has a total of %.2f. A good day's work.\n", livetotal+(*shop).cash);
+	shop->cash=livetotal+(*shop).cash;
 }
 
 //////////////////////////////////////////////////////////////////
@@ -221,8 +246,5 @@ void doingItLive(struct Shop shop)
 int main(void)
 {
 	struct Shop shop = createAndStockShop();
-	//printf("%d\n",shop.index);
-	//printf("%.2f\n",shop.cash);
-	//printf("%d\n",shop.stock[1].quantity);
-	doingItLive(shop);
+	doingItLive(&shop);
 }
